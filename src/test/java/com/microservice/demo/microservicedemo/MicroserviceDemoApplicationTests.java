53a0ddc78e5c4de2f64f33033cd080f9b2810374
Wei Seng <tanvincent999@gmail.com>
2022-02-09 17:09:43 +0800

    Initial commit

---
+++ src/test/java/com/microservice/demo/microservicedemo/MicroserviceDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.demo.microservicedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
