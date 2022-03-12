5609d2391b7cd413404bd153c1334df0d924e948
Wei Seng <tanvincent999@gmail.com>
2022-03-12 17:38:41 +0800

    Add Mock Twitter Services

--- twitter-to-kafka-service/src/main/java/com/microservice/demo/twitter/to/kafka/service/runner/impl/TwitterKafkaStreamRunner.java
+++ twitter-to-kafka-service/src/main/java/com/microservice/demo/twitter/to/kafka/service/runner/impl/TwitterKafkaStreamRunner.java
@@ -6,6 +6,7 @@ import com.microservice.demo.twitter.to.kafka.service.listener.TwitterKafkaStatu
 import com.microservice.demo.twitter.to.kafka.service.runner.StreamRunner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 import twitter4j.FilterQuery;
 import twitter4j.TwitterException;
@@ -16,6 +17,7 @@ import javax.annotation.PreDestroy;
 import java.util.Arrays;
 
 @Component
+@ConditionalOnProperty(name = "twitter-to-kafka-service.enable-mock-tweets", havingValue = "false", matchIfMissing = true)
 public class TwitterKafkaStreamRunner implements StreamRunner {
 
     private final TwitterToKafkaServiceConfigData twitterToKafkaServiceConfigData;
